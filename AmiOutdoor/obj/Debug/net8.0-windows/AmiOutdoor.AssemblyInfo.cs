68d28a26cbacbf62be49ad50b01e713b375c75fc
Ismael VAS <ismael.vas@Digital-Grenoble.local>
2024-08-07 16:22:34 +0200

    reset

--- AmiOutdoor/obj/Debug/net8.0-windows/AmiOutdoor.AssemblyInfo.cs
+++ AmiOutdoor/obj/Debug/net8.0-windows/AmiOutdoor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmiOutdoor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01306e5882488dfc4d1861471454317ea5a5a8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ad8455a8bde43ad96cc9dc58cd8b0d993c76b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmiOutdoor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmiOutdoor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
