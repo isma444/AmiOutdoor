41c9cf0bf0c6bac5dfdbb8edff65545648f82b79
Ismael VAS <ismael.vas@Digital-Grenoble.local>
2024-08-08 11:27:54 +0200

    work on WPf

---
+++ AmiOutdoor/ViewModel/AgendaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using AmiLibrary.Models;
+using System.ComponentModel;
+using AmiLibrary.Services;
+using System.Text.Json.Serialization;
+
+
+
+namespace AmiOutdoor.ViewModel
+{
+
+    class AgendaViewModel 
+    {
+       
+    }
+}
+
---
+++ AmiOutdoor/ViewModel/DateSelectionViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmiOutdoor.ViewModel
+{
+    internal class DateSelectionViewModel : INotifyPropertyChanged
+    {
+        private DateTime? _startDate;
+        private DateTime? _endDate;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DateTime? StartDate
+        {
+            get => _startDate;
+            set
+            {
+                if (_startDate != value)
+                {
+                    _startDate = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(_startDate));
+                }
+            }
+        }
+
+        public DateTime? EndDate
+        {
+            get => _endDate;
+            set
+            {
+                if (_endDate != value)
+                {
+                    _endDate = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(_endDate));
+                }
+            }
+        }
+
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
